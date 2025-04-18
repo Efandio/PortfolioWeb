8362fe560092ff66e72b77ec33f491dbce02d6c0
Efandio <efandio904@gmail.com>
2025-04-18 14:46:09 +0700

    skills section

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import AboutMe from "./components/AboutMe"
 import HeroSection from "./components/HeroSection"
+import MySkillsSection from "./components/MySkillsSection"
 import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
-    <main className="box-border text-white w-screen h-screen flex flex-collg:pb-0">
-      <Navbar aboutMe="#aboutMe" className="px-5 py-10" />
+    <main className="box-border text-white w-screen h-screen flex flex-collg:pb-0 overflow-x-hidden">
+      <Navbar aboutMe="#aboutMe" skills="#skills" className="px-5 py-10" />
 
       <section className="w-full h-full">
         <HeroSection className="mt-26 lg:mt-30" />
 
         <AboutMe id="aboutMe" className="mt-5 lg:mt-10" />
+
+        <MySkillsSection id="skills" className="mt-16 lg:mt-18" />
       </section>
     </main>
   )
--- src/components/MySkillsSection.tsx
+++ src/components/MySkillsSection.tsx
@@ -1,10 +1,45 @@
+import { PanelsTopLeft } from "lucide-react";
+import { Badge } from "./ui/badge";
+import { Card, CardContent } from "./ui/card";
 
+const mySkills = [
+    {id: 1, name: 'Frontend Development', description: 'Creating responsive and intuitive user interfaces with modern frameworks and libraries.', skills: ['HTML/CSS', 'JavaScript / TypeScript', 'React', 'Vue', 'Tailwind CSS']}
+];
 
-const MySkillsSection = () => {
+interface MySkills {
+    id: string;
+    className?: string;
+};
+
+const MySkillsSection: React.FC<MySkills> = ({ id, className }) => {
     return (
-        <>
-        
-        </>
+        <main id={id} className={`container w-full h-fit flex flex-col gap-8 ${className}`}>
+            <span className="w-full h-fit flex justify-center">
+                <Badge className="justify-center px-5 text-base">Skills</Badge>
+            </span>
+
+            <section className="grid grid-cols-1 justify-items-center ">
+                {mySkills.map((skill) => (
+                <Card className="w-[70vw] lg:w-[25vw]">
+                    <CardContent className="flex flex-col gap-2">
+                    <div className="bg-gray-200 h-fit w-fit p-2 rounded-full">
+                        <PanelsTopLeft size={28} />
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <h3 className="font-semibold text-xl">{skill.name}</h3>
+                        <p className="leading-4 text-xs lg:text-sm text-black/50">{skill.description}</p>
+                        <div className="grid grid-cols-2 gap-2 w-40 pt-1">
+                            {skill.skills.map((i) => (
+                                <Badge className="text-[8px] lg:text-[10px] w-fit">{i}</Badge>
+                            ))}
+                        </div>
+                    </div>
+                </CardContent>
+                </Card>
+                ))}
+            </section>
+        </main>
     )
 };
 
