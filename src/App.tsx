583db60f34642c37d32913e98045214d2cb6c88a
Efandio <efandio904@gmail.com>
2025-04-18 13:47:13 +0700

    about section

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ function App() {
 
   return (
     <main className="box-border text-white w-screen h-screen flex flex-collg:pb-0">
-      <Navbar className="px-5 py-10" />
+      <Navbar aboutMe="#aboutMe" className="px-5 py-10" />
 
-      <HeroSection className="mt-26 lg:mt-30" />
+      <section className="w-full h-full">
+        <HeroSection className="mt-26 lg:mt-30" />
 
-      <AboutMe />
+        <AboutMe id="aboutMe" className="mt-5 lg:mt-10" />
+      </section>
     </main>
   )
 }
--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,9 +1,16 @@
 
-const AboutMe = () => {
+interface HeroSectionProps {
+    id: string;
+    className?: string;
+};
+
+const AboutMe: React.FC<HeroSectionProps> = ({ id, className }) => {
     return (
-        <>
-        
-        </>
+        <main id={id} className={`w-full h-fit py-5 space-y-5 ${className}`}>
+            <h1 className="text-center lg:text-xl font-semibold underline underline-offset-3">About Me</h1>
+
+            <p className="text-sm text-balance w-full text-center items-center px-4 leading-6">Hey! I'm a self-taught web developer who started learning out of pure curiosity — and ended up loving the process of building things on the web.I specialize in creating responsive, clean, and user-friendly interfaces using React, Vue, Tailwind CSS, JavaScript, and TypeScript. Whether it's building a UI from scratch or solving tricky frontend logic, I enjoy turning ideas into functional and polished websites.I'm constantly learning and improving, and I'm currently open to full-time roles as well as freelance opportunities. I value clean code, good communication, and delivering work that not only works well — but also looks great.Let's build something awesome together.</p>
+        </main>
     )
 };
 
