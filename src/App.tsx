0b905b12e67c9ef8385852dc2bcf2683b18e33b6
Efandio <efandio904@gmail.com>
2025-04-18 10:40:47 +0700

    navbar dekstop

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <main className="text-white w-screen h-screen">
+      <Navbar />
+    </main>
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,37 @@
+import { useState } from 'react';
+import { Button } from "./ui/button";
 
-const Navbar = () => {
+interface NavbarProps {
+    aboutMe?: string;
+    skills?: string;
+    portfolio?: string;
+    contact?: string;
+};
+
+const Navbar: React.FC<NavbarProps> = ({ aboutMe, skills, portfolio, contact }) => {
+
+    const [ activeButton, setActiveButton ] = useState<'' | 'aboutMe' | 'skills' | 'portfolio' | 'contact'>('')
+
+    const handleClick = (button: 'aboutMe' | 'skills' | 'portfolio' | 'contact') => {
+        setActiveButton((prev) => prev === button ? '' : button);
+    }
+
     return (
         <>
-        
+            <nav className={`navbar bg-gray-200 flex items-center lg:justify-evenly w-full h-16 lg:px-20 fixed`}>
+                <a href={aboutMe}>
+                    <Button onClick={() => handleClick('aboutMe')} className={`${activeButton === 'aboutMe' ? '-translate-y-0 border-b-0' : '-translate-y-0.5 border-b-3 border-slate-500'}`} size='sm'>About Me</Button>
+                </a>
+                <a href={skills}>
+                    <Button onClick={() => handleClick('skills')} className={`${activeButton === 'skills' ? '-translate-y-0' : '-translate-y-0.5 border-b-3 border-slate-500'}`} size='sm'>Skills</Button>
+                </a>
+                <a href={portfolio}>
+                    <Button onClick={() => handleClick('portfolio')} className={`${activeButton === 'portfolio' ? '-translate-y-0' : '-translate-y-0.5 border-b-3 border-slate-500'}`} size='sm'>Portfolio</Button>
+                </a>
+                <a href={contact}>
+                    <Button onClick={() => handleClick('contact')} className={`${activeButton === 'contact' ? '-translate-y-0' : '-translate-y-0.5 border-b-3 border-slate-500'}`} size='sm'>Contact</Button>
+                </a>
+            </nav>
         </>
     )
 };
