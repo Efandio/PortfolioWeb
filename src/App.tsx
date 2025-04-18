c1b84de3f0419e0a55976d9296741ef49c3530d8
Efandio <efandio904@gmail.com>
2025-04-18 13:17:54 +0700

    hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import HeroSection from "./components/HeroSection"
 import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
-    <main className="box-border text-white w-screen h-screen flex flex-col-reverse lg:flex-col pb-10 lg:pb-0">
-      <Navbar className="px-5" />
+    <main className="box-border text-white w-screen h-screen flex flex-collg:pb-0">
+      <Navbar className="px-5 py-10" />
+
+      <HeroSection className="mt-26 lg:mt-30" />
     </main>
   )
 }
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,9 +1,29 @@
+import { Button } from "./ui/button";
 
-const HeroSection = () => {
+interface HeroSectionProps {
+    className?: string;
+};
+
+const HeroSection: React.FC<HeroSectionProps> = ({ className }) => {
     return (
-        <>
-        
-        </>
+        <main className={`flex flex-col gap-8 lg:gap-14 items-center w-full h-fit py-10 ${className}`}>
+        {/* Nama & Job Title */}
+            <section className="flex flex-col w-full justify-center items-center">
+                <span className="w-full translate-x-12 font-bold italic lg:hidden">Hello,</span>
+                <h1 className="font-bold text-xl font-serif pb-2 flex lg:text-4xl"><span className="hidden lg:flex pr-1">Hello,</span>I'm Efandio Attahya Nofianto</h1>
+                <h2 className="text-white/90 lg:text-xl">I'm a Frontend Developer</h2>
+            </section>
+        {/* View CV */}
+            <section className="flex gap-5 lg:gap-18">
+                <a href="portfolio">
+                    <Button>View My Work</Button>
+                </a>
+
+                <a href="resume">
+                    <Button>Download Resume</Button>
+                </a>
+            </section>
+        </main>
     )
 };
 
