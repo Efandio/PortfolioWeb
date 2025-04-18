5d763a817700e2c3801e40808683412eeda7ec55
Efandio <efandio904@gmail.com>
2025-04-18 22:45:48 +0700

    contact section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import AboutMe from "./components/AboutMe"
+import ContactSection from "./components/ContactSection"
 import HeroSection from "./components/HeroSection"
 import MySkillsSection from "./components/MySkillsSection"
 import Navbar from "./components/Navbar"
@@ -8,16 +9,18 @@ function App() {
 
   return (
     <main className="box-border text-white w-screen h-screen flex flex-collg:pb-0 overflow-x-hidden">
-      <Navbar aboutMe="#aboutMe" skills="#skills" projects="#projects" className="px-5 py-10" />
+      <Navbar aboutMe="#aboutMe" skills="#skills" projects="#projects" contact="#contact" className="px-5 py-10" />
 
       <section className="w-full h-full">
-        <HeroSection className="mt-26 lg:mt-30" />
+        <HeroSection className="pt-26 lg:pt-30" />
 
-        <AboutMe id="aboutMe" className="mt-5 lg:mt-10" />
+        <AboutMe id="aboutMe" className="pt-5 lg:pt-10" />
 
-        <MySkillsSection id="skills" className="mt-16 lg:mt-18" />
+        <MySkillsSection id="skills" className="pt-16 lg:pt-18" />
 
-        <ProjectsSection id="projects" className="mt-16 lg:mt-22" />
+        <ProjectsSection id="projects" className="pt-16 lg:pt-22" />
+
+        <ContactSection id="contact" className="pt-22 lg:pt-26" />
       </section>
     </main>
   )
